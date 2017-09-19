9384dc51b7ce6375a49c12077bfcf00bb71fd7a8
Isak Mladenoff <imladenoff@gmail.com>
2017-09-19 10:50:13 -0700

    Set up webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: '.frontend/bearbnb.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
